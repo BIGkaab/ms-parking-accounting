eba86f72181ff1299bde23d8484e87e6ce33800b
Kiven Acevedo <kiven.acv2@gmail.com>
2022-04-11 11:37:01 -0400

    first commit

---
+++ ms-parking-accounting/src/main/java/com/parking/accounting/MsParkingAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.parking.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsParkingAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsParkingAccountingApplication.class, args);
+	}
+
+}
