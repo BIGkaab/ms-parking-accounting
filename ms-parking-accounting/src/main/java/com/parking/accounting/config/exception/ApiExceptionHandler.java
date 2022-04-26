1a0facbdb80502c49731199c38944c38c40cc749
Kiven Acevedo <kiven.acv2@gmail.com>
2022-04-26 15:13:45 -0400

    IntersalServerErrorException implementado en los caso de uso

--- ms-parking-accounting/src/main/java/com/parking/accounting/config/exception/ApiExceptionHandler.java
+++ ms-parking-accounting/src/main/java/com/parking/accounting/config/exception/ApiExceptionHandler.java
@@ -3,14 +3,15 @@ package com.parking.accounting.config.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class ApiExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = BadRequestException.class)
+    @ExceptionHandler(value = BadRequestException.class)
     public ResponseEntity<Object> badRequestException(BadRequestException e){
         HttpStatus badRequest = HttpStatus.BAD_REQUEST;
         ResponseException responseException = new ResponseException(
@@ -19,5 +20,16 @@ public class ExceptionHandler {
                 ZonedDateTime.now(ZoneId.of("Z"))
         );
         return new ResponseEntity<>(responseException, badRequest);
+    }
+
+    @ExceptionHandler(value = InternalServerErrorException.class)
+    public ResponseEntity<Object> internalServerErrorException(InternalServerErrorException e){
+        HttpStatus internalServerError = HttpStatus.INTERNAL_SERVER_ERROR;
+        ResponseException responseException = new ResponseException(
+                e.getMessage(),
+                internalServerError,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+        return new ResponseEntity<>(responseException, internalServerError);
     }
 }
